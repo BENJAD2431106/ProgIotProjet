efd6a55c04ae9aaa18e7a811b21a0732e1ea8fdf
Allyson Gagnon-Lefebvre <2433452@etu.cegepjonquiere.ca>
2025-10-21 10:53:18 -0400

    Ajout script

---
+++ Scripts_SQL/ProcedureStokerConnexionProd.sql
@@ -0,0 +1,22 @@
+USE PROG3A25_Production_AllysonJad;
+GO
+CREATE PROCEDURE UP_ConnexionUtilisateur 
+	@courriel NVARCHAR(255), 
+	@motDePasse NVARCHAR(255),
+	@noUtiliateur INT OUTPUT -- changer ça
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DECLARE @sel UNIQUEIDENTIFIER
+
+	IF NOT EXISTS(SELECT * FROM Utilisateurs WHERE @courriel=courriel)
+		BEGIN
+			SET @noUtiliateur = -1; 
+		RETURN;
+	END 
+	ELSE 
+		SET @noUtiliateur =(SELECT noUtilisateur FROM Utilisateurs WHERE @noUtiliateur = HASHBYTES('SHA2_512', @motDePasse + CAST(@sel AS NVARCHAR(36))));
+END;
+
+exec UP_ConnexionUtilisateur
