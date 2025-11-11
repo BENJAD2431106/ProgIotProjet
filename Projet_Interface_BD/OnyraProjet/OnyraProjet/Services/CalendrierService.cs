436e99af245df2876e558b9e50c07a655a6c43d9
Jad Bentayaa Idrissi <2431106@etu.cegepjonquiere.ca>
2025-11-11 15:44:41 -0500

    modifcalendar

--- Projet_Interface_BD/OnyraProjet/OnyraProjet/Services/CalendrierService.cs
+++ Projet_Interface_BD/OnyraProjet/OnyraProjet/Services/CalendrierService.cs
@@ -1,6 +1,37 @@
-﻿namespace OnyraProjet.Services
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using OnyraProjet.Data;
+using OnyraProjet.Models;
+using OnyraProjet.Partials;
+
+namespace OnyraProjet.Services
 {
     public class CalendrierService
     {
+
+        private readonly IDbContextFactory<Prog3a25ProductionAllysonJadContext> myCalendarFactory;
+
+        public CalendrierService(IDbContextFactory<Prog3a25ProductionAllysonJadContext> myFactory)
+        {
+            this.myCalendarFactory = myFactory;
+        }
+        public async Task AjouterCalendrier(Calendrier calendrier)
+        {
+            var dbContext = myCalendarFactory.CreateDbContextAsync().Result;
+
+            try
+            {
+                dbContext.Calendriers.Add(calendrier);
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
     }
 }
+
+
