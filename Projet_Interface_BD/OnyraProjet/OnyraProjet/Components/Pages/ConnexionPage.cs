63d7cb7de97059a9f13f65f1fb64bc30fb6ddd30
Allyson Gagnon-Lefebvre <2433452@etu.cegepjonquiere.ca>
2025-11-14 09:14:43 -0500

    ajout

---
+++ Projet_Interface_BD/OnyraProjet/OnyraProjet/Components/Pages/ConnexionPage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using OnyraProjet.Models;
+using OnyraProjet.Services;
+using System.Threading.Tasks;
+
+namespace OnyraProjet.Pages
+{
+    public class ConnexionPage : ComponentBase
+    {
+        [Inject]
+        public ConnexionService ConnexionService { get; set; }
+
+        protected ConnexionModel infos = new();
+        protected string message = "";
+
+        protected async Task SeConnecter()
+        {
+            int resultat = await ConnexionService.UP_ConnexionUtilisateur(infos.AdresseCourriel, infos.MotDePasse);
+
+            if (resultat == -1)
+                message = "Courriel ou mot de passe incorrect !";
+            else
+                message = $"Connexion réussie !";
+        }
+    }
+}
